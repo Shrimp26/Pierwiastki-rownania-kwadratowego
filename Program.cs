3f02a839032f306cd382a258904e748e79989bef
julia <julia@LAPTOP-ILMLJSM->
2024-02-11 11:52:05 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+//Napisz program, który oblicza pierwiastki równania kwadratowego ax2 + bx + c = 0.
+//Zmienne a, b, c to liczby rzeczywiste wprowadzane z klawiatury przez użytkownika.
+//Wszystkie zmienne wyświetl na ekranie z dokładnością do dwóch miejsc po przecinku.
+
+class PierwiastkiRownaniaKwadratowego
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("obliczanie pierwiastków równania kwadratowego ax2 + bx + c = 0.");
+        Console.WriteLine("Podaj wartość dla a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj wartość dla b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj wartość dla c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        double delta = b * b - 4 * a * c;
+
+        if (delta > 0)
+        {
+            double sqrtDelta = Math.Sqrt(delta);
+            double x1 = (-b + sqrtDelta) / (2 * a);
+            double x2 = (-b + sqrtDelta) / (2 * a);
+            Console.WriteLine($"Równanie ma dwa pierwiastki rzeczywiste: x1 = {Math.Round(x1, 2)}, x2 = {Math.Round(x2, 2)}.");
+        }
+        else if (delta == 0)
+        {
+            double x = -b / (2 * a);
+            Console.WriteLine($"Równanie ma jeden podwójny pierwiastek rzeczywisty: x = {Math.Round(x, 2)}.");
+        }
+        else
+        {
+            Console.WriteLine("Równanie nie ma pierwiastków rzeczywistych.");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Pierwiastki równania kwadratowego")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pierwiastki równania kwadratowego")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("9e0544f2-fbe1-4caa-af44-8958af5acc77")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
